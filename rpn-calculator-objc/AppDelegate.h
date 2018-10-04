1fb84aa5e45b519eddc90eca9091151318dfd12c
stdemicheli <stdemicheli@gmail.com>
2018-10-04 23:17:10 +0200

    Initial commit.

---
+++ rpn-calculator-objc/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rpn-calculator-objc
+//
+//  Created by De MicheliStefano on 04.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rpn-calculator-objc/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  rpn-calculator-objc
+//
+//  Created by De MicheliStefano on 04.10.18.
+//  Copyright © 2018 De MicheliStefano. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
